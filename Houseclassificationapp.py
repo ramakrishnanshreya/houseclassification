620e849c7004b34068fcd4a0707bbe090e0c194d
Shreya Ramakrishnan <hi22sr@iiphh.org>
2023-12-15 23:12:35 +0530

    Update Houseclassificationapp.py

--- Houseclassificationapp.py
+++ Houseclassificationapp.py
@@ -23,31 +23,33 @@ def classify_image(image_path):
     return class_label, confidence
 
 # Function to classify all images in a folder
-def classify_images(image_paths):
+def classify_images(image_paths, output_folder):
     results = []
     for image_path in image_paths:
-        try:
-            class_label, confidence = classify_image(image_path)
-            results.append({
-                'filename': os.path.basename(image_path),
-                'class_label': class_label,
-                'confidence': confidence
-            })
-        except Exception as e:
-            st.warning(f"Error classifying image {image_path}: {e}")
+        # Check if the path is a file and ends with a supported image extension
+        if os.path.isfile(image_path) and image_path.lower().endswith(('.jpg', '.jpeg', '.png')):
+            try:
+                class_label, confidence = classify_image(image_path)
+                results.append({
+                    'filename': os.path.basename(image_path),
+                    'class_label': class_label,
+                    'confidence': confidence
+                })
+
+                # Create a folder for each class
+                class_folder = os.path.join(output_folder, class_label)
+                os.makedirs(class_folder, exist_ok=True)
+
+                # Move the image to the corresponding class folder
+                new_path = os.path.join(class_folder, os.path.basename(image_path))
+                os.rename(image_path, new_path)
+
+            except Exception as e:
+                st.warning(f"Error classifying image {image_path}: {e}")
+        else:
+            st.warning(f"Skipping non-image file: {image_path}")
 
     return results
-
-# Function to rename images based on classifications
-def rename_images(image_paths, results):
-    renamed_paths = []
-    for result in results:
-        old_path = os.path.join(extraction_path, result['filename'])
-        new_filename = f"{result['class_label']}_{result['confidence']:.2%}.jpg"
-        new_path = os.path.join(extraction_path, new_filename)
-        os.rename(old_path, new_path)
-        renamed_paths.append(new_path)
-    return renamed_paths
 
 # Create Streamlit app
 st.title("Image Classification App")
@@ -55,21 +57,22 @@ st.title("Image Classification App")
 uploaded_zip = st.file_uploader("Upload a zip file containing images", type=["zip"])
 
 if uploaded_zip is not None:
+    # Create an output folder to organize images by class
+    output_folder = "output_images"
+    os.makedirs(output_folder, exist_ok=True)
+
     # Extract the zip file
     with zipfile.ZipFile(uploaded_zip, 'r') as zip_ref:
         extraction_path = "temp_images"
         zip_ref.extractall(extraction_path)
 
-    st.write(f"Classifying and renaming images in folder: {extraction_path}")
+    st.write(f"Classifying and organizing images in folder: {extraction_path}")
 
     # Get the list of image paths in the extracted folder
     image_paths = [os.path.join(extraction_path, filename) for filename in os.listdir(extraction_path)]
 
-    # Perform inference on all images in the folder
-    results = classify_images(image_paths)
-
-    # Rename images based on classifications
-    renamed_paths = rename_images(image_paths, results)
+    # Perform inference on all images in the folder and organize by class
+    results = classify_images(image_paths, output_folder)
 
     # Display results
     for result in results:
@@ -80,7 +83,7 @@ if uploaded_zip is not None:
     # Create a DataFrame from the results
     df = pd.DataFrame(results)
 
-    # Add a download button for the results
+    # Add a download button for the CSV file with image names and classes
     st.download_button(
         label="Download Results as CSV",
         data=df.to_csv(index=False).encode('utf-8'),
@@ -88,18 +91,22 @@ if uploaded_zip is not None:
         key='download_results_button'
     )
 
-    # Create a new zip file with renamed images
-    with zipfile.ZipFile("renamed_images.zip", 'w') as zip_output:
-        for renamed_path in renamed_paths:
-            zip_output.write(renamed_path, os.path.basename(renamed_path))
-    
-    # Add a download button for the renamed images
+    # Create a zip file containing the organized images
+    organized_zip_path = "organized_images.zip"
+    with zipfile.ZipFile(organized_zip_path, 'w') as zip_output:
+        for root, dirs, files in os.walk(output_folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zip_output.write(file_path, os.path.relpath(file_path, output_folder))
+
+    # Add a download button for the zip file with organized images
     st.download_button(
-        label="Download Renamed Images",
-        data=open("renamed_images.zip", "rb").read(),
-        file_name='renamed_images.zip',
-        key='download_renamed_button'
+        label="Download Organized Images",
+        data=open(organized_zip_path, "rb").read(),
+        file_name='organized_images.zip',
+        key='download_organized_button'
     )
+
 
 
 
